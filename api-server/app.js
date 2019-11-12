909d974c54548d9f3cbc051b06fe4eacc6ddc926
Logqwerty <meg0070@naver.com>
2019-11-12 18:26:06 +0900

    [Feat] back-end 디렉토리 구조 생성
    close #3

--- api-server/app.js
+++ api-server/app.js
@@ -1,10 +1,41 @@
 const express = require('express');
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+const cors = require('cors');
 
 const app = express();
+
+app.use(
+  session({
+    secret: '@#@youth$romantic$flex@#@',
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/', (req, res) => {
   res.send('hello express');
 });
-app.listen(4000, () => {
-  console.log('connected on port 4000');
+
+app.use((req, res, next) => {
+  next(createError(404));
 });
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500).json({});
+});
+
+module.exports = app;
