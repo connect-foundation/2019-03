4b7df4507a738ab39db2b14b0a57d581bdec197b
남정호 <40619551+njh7799@users.noreply.github.com>
2019-11-13 14:38:52 +0900

    [Feat] styled-component 설정 (#39)
    CSS를 관리하기 위하여 styled-component 설정.
    자주 쓰는 색상들을 theme에 저장.
    styled-component를 더 원활히 사용하기 위해 polished 패키지 설치.

--- web/src/containers/App/index.js
+++ web/src/containers/App/index.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+
+const AppWrapper = styled.div`
+  max-width: calc(768px + 16px * 2);
+  margin: 0 auto;
+  display: flex;
+  min-height: 100%;
+  padding: 0 16px;
+  flex-direction: column;
+`;
 
 export default function App() {
-  return <div className="App">React App</div>;
+  return (
+    <ThemeProvider
+      theme={{
+        palette: {
+          gray_background: 'FAFAFA',
+          gray_button: 'FDFDFD',
+          gray_font: '999999',
+          border: 'B2B2B2',
+          white: 'FFFFFF',
+          blue: '3897F1',
+          blue_facebook: '375184',
+        },
+      }}
+    >
+      <AppWrapper>React App</AppWrapper>
+    </ThemeProvider>
+  );
 }
