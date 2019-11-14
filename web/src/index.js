7baca97a2bb322d5e5c33065be1c81d8dbf90252
Jack <rainy_waltz@naver.com>
2019-11-14 14:44:17 +0900

    [Feat] React Router 추거
    React Router를 이용하여 페이지 구현.

--- web/src/index.js
+++ web/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
