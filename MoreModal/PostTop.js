70641a42d5a0323ce180948010a40a9730cfbbc7
soyoungjeong <hana46798@gmail.com>
2019-11-15 11:36:08 +0900

    [정소영] w2_리뷰요청_모달 UI 구현

---
+++ MoreModal/PostTop.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Wrapper from "./Wrapper";
+import MoreIcon from "./MoreIcon";
+import Profile from "../ProfileIcon";
+import StyledLink from "./StyledLink";
+import MoreModal from "./MoreModal";
+
+const PostTop = ({ writer, myInfo }) => {
+  const [moreModal, setMoreModal] = useState(false);
+  const clickMore = () => {
+    setMoreModal(true);
+  };
+
+  return (
+    <>
+      <Wrapper>
+        <StyledLink to={`/${writer.username}`}>
+          <div className="user">
+            <Profile />
+            <span className="username">{writer.username}</span>
+          </div>
+        </StyledLink>
+        <div className="more">
+          <MoreIcon onClick={clickMore} />
+        </div>
+      </Wrapper>
+      <MoreModal visible={moreModal} writer={writer} myInfo={myInfo} />
+    </>
+  );
+};
+export default PostTop;
