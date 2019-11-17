c851c2b8f609cddd361834db328960f34e292f20
Logqwerty <meg0070@naver.com>
2019-11-17 20:31:48 +0900

    [Add] .sequelizerc 내용 추가 및 sequelize-config 파일 추가
    migrations, seeders, models, config 폴더의 경로를 지정하기 위해
    .sequelizerc 내용 추가.
    
    sequelize에서 사용할 환경설정 값들을 정의하기 위해 sequelize-config
    설정파일 추가.

---
+++ api-server/config/sequelize-config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DB_NAME_DEV,
+    host: process.env.DB_HOST_DEV,
+    dialect: process.env.DB_DIALECT_DEV,
+  },
+  test: {
+    username: process.env.DB_USER_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST_TEST,
+    dialect: process.env.DB_DIALECT_TEST,
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+};
