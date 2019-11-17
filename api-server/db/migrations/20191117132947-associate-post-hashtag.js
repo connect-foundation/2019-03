e1afebea341f5d18593dbda2f9dbe297ea55c8a8
Logqwerty <meg0070@naver.com>
2019-11-17 22:52:18 +0900

    [Fix] Post와 HashTag 간 외래키 조건 추가(HashTagsOfPosts)

--- api-server/db/migrations/20191117132947-associate-post-hashtag.js
+++ api-server/db/migrations/20191117132947-associate-post-hashtag.js
@@ -21,10 +21,18 @@ module.exports = {
       PostId: {
         allowNull: false,
         type: Sequelize.UUID,
+        references: {
+          model: 'Posts',
+          key: 'id',
+        },
       },
       HashTagId: {
         allowNull: false,
         type: Sequelize.UUID,
+        references: {
+          model: 'HashTags',
+          key: 'id',
+        },
       },
     });
   },
