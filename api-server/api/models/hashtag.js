a60288b2a37a4d8b7fee52a70b305431511021ae
Logqwerty <meg0070@naver.com>
2019-11-17 22:20:55 +0900

    [Feat] HashTag 모델 추가
    해시태그를 나타내는 HashTag 모델 추가.

---
+++ api-server/api/models/hashtag.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const HashTag = sequelize.define(
+    'HashTag',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      name: {
+        type: DataTypes.STRING(45),
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {},
+  );
+  HashTag.associate = function(models) {
+    // associations can be defined here
+  };
+  return HashTag;
+};
---
+++ api-server/db/migrations/20191117131642-create-hash-tag.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('HashTags', {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      name: {
+        type: Sequelize.STRING(45),
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('HashTags');
+  },
+};
