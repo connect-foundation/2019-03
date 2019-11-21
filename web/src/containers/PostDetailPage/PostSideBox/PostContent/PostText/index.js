8f38d5b293260c49109f366f0a5eba5c7c8cac2a
Jack <rainy_waltz@naver.com>
2019-11-21 16:11:35 +0900

    [Refact] 사용하지 않는 파일 삭제
    사용하는 않는 파일 제가.

--- web/src/containers/PostDetailPage/PostSideBox/PostContent/PostText/index.js
+++ web/src/containers/PostDetailPage/PostSideBox/PostContent/PostText/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import { CommentWrapper, ProfileWrapper, LikeIconWrapper } from './styles';
+import { CommentWrapper, ProfileWrapper } from './styles';
 import ProfileIcon from '../../../../../components/ProfileIcon';
-import LikeIcon from '../../../../../components/LikeIcon';
 import Content from './Content';
 
 const Comment = ({ user, content, updatedAt, likerCount, commentId }) => {
