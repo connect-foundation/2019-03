af12f97f3a326a60668d8612314a3e04fb3e5f0f
JaeHyeonKim19 <JaeHyeonKim19@gmail.com>
2019-11-21 12:11:56 +0900

    [Fix] followStatus prop 추가
    FollowButton에서 팔로우 여부를 확인하는 fetch요청 시, 버튼 수만큼
    fetch요청이 생기는 등 많은 개선사항이 발생. 따라서 상위
    컴포넌트에서 팔로우 여부를 확인하는 followStatus를 소유하고 자식
    컴포넌트에게 내려주는 방식으로 구현.

--- web/src/components/FollowButton/index.js
+++ web/src/components/FollowButton/index.js
@@ -2,29 +2,36 @@ import React, { useState } from 'react';
 
 import StyledFollowButton from './StyledFollowButton';
 
-const FollowButton = () => {
-  const [followStatus, setFollowStatus] = useState('팔로우');
+const FollowButton = ({ followStatus }) => {
+  const [currentFollowStatus, setCurrentFollowStatus] = useState(followStatus);
   const changeFollowStatus = () => {
-    switch (followStatus) {
+    switch (currentFollowStatus) {
       case '팔로우':
-        setFollowStatus('요청됨');
+        setCurrentFollowStatus('요청됨');
         break;
       case '요청됨':
-        setFollowStatus('팔로잉');
+        setCurrentFollowStatus('팔로잉');
         break;
       case '팔로잉':
-        setFollowStatus('팔로우');
+        setCurrentFollowStatus('팔로우');
         break;
       default:
-        setFollowStatus('오류. 새로고침해주십시오');
+        setCurrentFollowStatus('오류. 새로고침해주십시오');
         break;
     }
   };
   return (
-    <StyledFollowButton status={followStatus} onClick={changeFollowStatus}>
-      {followStatus}
+    <StyledFollowButton
+      status={currentFollowStatus}
+      onClick={changeFollowStatus}
+    >
+      {currentFollowStatus}
     </StyledFollowButton>
   );
+};
+
+FollowButton.defaultProps = {
+  followStatus: '팔로우',
 };
 
 export default FollowButton;
