93eb9df9b795c4f47d97c059d62e05e079c5988d
Jack <rainy_waltz@naver.com>
2019-11-21 13:44:01 +0900

    [Refact] 페이지UI 변경
    상세 페이지에서 스크롤이 생기지 않게끔 포스트 크기 수정.
    네비게이션바와 너무 벌어져있던 것을 수정.

--- web/src/containers/PostDetailPage/PostDetailPageWrapper.js
+++ web/src/containers/PostDetailPage/PostDetailPageWrapper.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const PostDetailPageWrapper = styled.div`
   width: 950px;
-  margin: 0 auto;
+  margin: -10px auto 16px auto;
   display: flex;
   align-items: stretch;
 `;
