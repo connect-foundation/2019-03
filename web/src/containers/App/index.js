aebb04362b6a27ca28c4df8711c85530c2c1fd6d
Jack <rainy_waltz@naver.com>
2019-11-23 09:48:10 +0900

    [Refact] 네이게이션 position 수정
    position를 fixed로 변경.
    배경 색을 white로 변경.
    태그를 nav 로 수정.

--- web/src/containers/App/index.js
+++ web/src/containers/App/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
+import AppWrapper from './AppWrapper';
 import HomePage from '../HomePage';
 import UserPage from '../UserPage';
 import PostDetailPage from '../PostDetailPage';
@@ -11,26 +12,28 @@ import Navigation from '../Navigation';
 export default function App() {
   const myInfo = { username: 'sam' };
   return (
-    <ThemeProvider
-      theme={{
-        palette: {
-          gray_background: '#FAFAFA',
-          gray_button: '#FDFDFD',
-          gray_font: '#999999',
-          border: '#e6e6e6',
-          border_secondary: '#dbdbdb',
-          white: '#FFFFFF',
-          blue: '#3897F1',
-          blue_facebook: '#375184',
-          pink: '#ee4957',
-        },
-      }}
-    >
-      <Navigation myInfo={myInfo} />
-      <Route path="/" exact component={HomePage} />
-      <Route path="/:username" component={UserPage} />
-      <Route path="/p/:postHash" component={PostDetailPage} />
-      <Route path="/newpost" component={NewPostPage} />
-    </ThemeProvider>
+    <AppWrapper>
+      <ThemeProvider
+        theme={{
+          palette: {
+            gray_background: '#FAFAFA',
+            gray_button: '#FDFDFD',
+            gray_font: '#999999',
+            border: '#e6e6e6',
+            border_secondary: '#dbdbdb',
+            white: '#FFFFFF',
+            blue: '#3897F1',
+            blue_facebook: '#375184',
+            pink: '#ee4957',
+          },
+        }}
+      >
+        <Navigation myInfo={myInfo} />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/:username" component={UserPage} />
+        <Route path="/p/:postHash" component={PostDetailPage} />
+        <Route path="/newpost" component={NewPostPage} />
+      </ThemeProvider>
+    </AppWrapper>
   );
 }
--- web/src/containers/Navigation/NavBackground.js
+++ web/src/containers/Navigation/NavBackground.js
@@ -1,13 +1,20 @@
 import styled from 'styled-components';
 
-const NavBackground = styled.div`
-  border-bottom: 1px solid ${({ theme }) => theme.palette.border_secondary};
+const NavBackground = styled.nav`
+  position: fixed;
+  left: 0;
+  top: 0;
+
+  width: 100%;
   height: 80px;
+  background-color: white;
+  border-bottom: 1px solid ${({ theme }) => theme.palette.border_secondary};
+  box-sizing: border-box;
+  padding: 0px 20%;
+
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  box-sizing: border-box;
-  padding: 0px 20%;
 `;
 
 export default NavBackground;
