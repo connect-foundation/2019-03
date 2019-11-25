aa9a16805bc95ee15631b94bcf8f58c00790713d
Jack <rainy_waltz@naver.com>
2019-11-25 13:56:49 +0900

    [Refact] reducer 와 context 추가
    reducer와 context를 사용하여 구조 개선.

--- web/src/containers/PostDetailPage/PostSideBox/index.js
+++ web/src/containers/PostDetailPage/PostSideBox/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import SideBoxWrapper from './SideBoxWrapper';
 
 import PostTop from '../../../components/PostTop';
 import PostContent from './PostContent';
 import CommentInput from '../../../components/CommentInput';
 import UtilityBlock from './UtilityBlock';
+import PostContext from '../context';
 
-function SideBox({ writer, myInfo, post }) {
+function SideBox() {
+  const { state } = useContext(PostContext);
+  const myInfo = { username: 'sam' };
   return (
     <SideBoxWrapper>
-      <PostTop writer={writer} myInfo={myInfo} post={post} />
+      <PostTop writer={state.post.writer} myInfo={myInfo} post={state.post} />
       <PostContent />
       <UtilityBlock />
       <CommentInput />
--- web/src/containers/PostDetailPage/index.js
+++ web/src/containers/PostDetailPage/index.js
@@ -1,26 +1,84 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { ThemeProvider } from 'styled-components';
-
 import { PostDetailPageWrapper, ViewPort } from './styles';
-
 import PostSideBox from './PostSideBox';
+import reducer from './reducer';
+import { PostProvider } from './context';
 
 function PostDetailPage() {
-  const postImg = new Image();
-  const writer = { username: 'queen', isFollow: true, profileURL: 'queen.jpg' };
-  const myInfo = { username: 'sam' };
-  const post = {
-    postHash: 'goddamn',
-    scr: 'http://image.chosun.com/sitedata/image/201705/11/2017051101043_0.jpg',
+  const initialState = {
+    post: {
+      content: '올 겨울 신상',
+      updatedAt: null,
+      writer: {
+        username: 'queen',
+        isFollow: true,
+        profileURL: 'queen.jpg',
+      },
+      imageURL:
+        'http://image.chosun.com/sitedata/image/201705/11/2017051101043_0.jpg',
+      postURL: null,
+    },
+    comments: [
+      {
+        user: {
+          username: 'jack',
+          profileURL: null,
+        },
+        content:
+          ' 11월 고고쉼 후원판매☃️ 여러분! 오늘은 이캠 하나스퀘어의 피아노 라운지 옆에서 고고쉼 후원판매를 진행하고 있으니 많이많이 가주세요!!',
+        updatedAt: null,
+        likers: 32,
+        commentId: 111,
+      },
+      {
+        user: {
+          username: 'sam',
+          profileURL: null,
+        },
+        content:
+          ' 11월 고고쉼 후원판매☃️ 여러분! 오늘은 이캠 하나스퀘어의 피아노 라운지 옆에서 고고쉼 후원판매를 진행하고 있으니 많이많이 가주세요!!',
+        updatedAt: null,
+        likers: 32,
+        commentId: 111,
+      },
+      {
+        user: {
+          username: 'sam',
+          profileURL: null,
+        },
+        content:
+          ' 11월 고고쉼 후원판매☃️ 여러분! 오늘은 이캠 하나스퀘어의 피아노 라운지 옆에서 고고쉼 후원판매를 진행하고 있으니 많이많이 가주세요!!',
+        updatedAt: null,
+        likers: 32,
+        commentId: 111,
+      },
+      {
+        user: {
+          username: 'pet',
+          profileURL: null,
+        },
+        content:
+          ' 11월 고고쉼 후원판매☃️ 여러분! 오늘은 이캠 하나스퀘어의 피아노 라운지 옆에서 고고쉼 후원판매를 진행하고 있으니 많이많이 가주세요!!',
+        updatedAt: null,
+        likers: 32,
+        commentId: 111,
+      },
+    ],
+    likerCounts: 100,
   };
-  postImg.src = post.scr;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const postImg = new Image();
+  postImg.src = state.post.imageURL;
   return (
-    <ThemeProvider theme={{ post_length: 600 }}>
-      <PostDetailPageWrapper>
-        <ViewPort img={postImg} />
-        <PostSideBox writer={writer} myInfo={myInfo} post={post} />
-      </PostDetailPageWrapper>
-    </ThemeProvider>
+    <PostProvider value={{ state, dispatch }}>
+      <ThemeProvider theme={{ post_length: 600 }}>
+        <PostDetailPageWrapper>
+          <ViewPort img={postImg} />
+          <PostSideBox />
+        </PostDetailPageWrapper>
+      </ThemeProvider>
+    </PostProvider>
   );
 }
 
