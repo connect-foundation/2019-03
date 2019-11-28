9e8f2744878e7943c1c177e75476d473a947b752
soyoungjeong <hana46798@gmail.com>
2019-11-28 16:05:48 +0900

    [Feat] 새게시물 사진 업로드, 미리보기 기능
    탐색기로 업로드 하고싶은 사진을 선택하면 미리보기 및 crop이 가능한
    기능 추가. react-easy-crop library 사용.
    
    closed #171 and closed #172

--- web/src/containers/NewPostPage/index.js
+++ web/src/containers/NewPostPage/index.js
@@ -1,15 +1,45 @@
 import React, { useState } from 'react';
+import Slider from '@material-ui/core/Slider';
+import Cropper from 'react-easy-crop';
 
-function NewPostPage() {
-  const [state, setstate] = useState(new FormData());
+function readFile(file) {
+  return new Promise(resolve => {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => resolve(reader.result), false);
+    reader.readAsDataURL(file);
+  });
+}
 
-  const inputFile = e => {
-    setstate(e.target.files[0]);
+const NewPostPage = () => {
+  const [file, setFile] = useState({
+    imageSrc: null,
+    crop: { x: 0, y: 0 },
+    zoom: 1,
+    aspect: 1 / 1,
+  });
+
+  const inputFile = async e => {
+    if (e.target.files && e.target.files.length > 0) {
+      const imageDataUrl = await readFile(e.target.files[0]);
+      setFile({ ...file, imageSrc: imageDataUrl });
+    }
+  };
+
+  const onCropChange = crop => {
+    setFile({ ...file, crop });
+  };
+
+  const onCropComplete = (croppedArea, croppedAreaPixels) => {
+    console.log(croppedArea, croppedAreaPixels);
+  };
+
+  const onZoomChange = zoom => {
+    setFile({ ...file, zoom });
   };
 
   const post = async () => {
     const formData = new FormData();
-    formData.append('file', state);
+    formData.append('file', file);
 
     try {
       const response = await fetch('http://localhost:4000/upload', {
@@ -19,14 +49,41 @@ function NewPostPage() {
       const { data } = await response.json();
     } catch (e) {}
   };
+
   return (
-    <div>
-      <input type="file" name="file" onChange={inputFile} />
-      <button type="button" onClick={post}>
-        submit
-      </button>
+    <div style={{ psotion: 'absolute' }}>
+      <input type="file" onChange={inputFile} />
+      {file.imageSrc && (
+        <>
+          <div
+            className="crop-container"
+            style={{ position: 'relative', width: '100%', height: '400px' }}
+          >
+            <Cropper
+              image={file.imageSrc}
+              crop={file.crop}
+              zoom={file.zoom}
+              aspect={file.aspect}
+              onCropChange={onCropChange}
+              onCropComplete={onCropComplete}
+              onZoomChange={onZoomChange}
+            />
+          </div>
+          <div className="controls">
+            <Slider
+              value={file.zoom}
+              min={1}
+              max={3}
+              step={0.1}
+              aria-labelledby="Zoom"
+              onChange={(e, zoom) => onZoomChange(zoom)}
+              classes={{ container: 'slider' }}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default NewPostPage;
