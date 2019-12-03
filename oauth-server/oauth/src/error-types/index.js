5e523a047ff5ecae62e432640ffc0d6a512cdd27
Logqwerty <meg0070@naver.com>
2019-12-03 18:07:02 +0900

    [Feat] AccessDeniedError 클래스 구현
    Error Response 중 access_denied에 대한 Error클래스 구현 및 테스트
    코드 구현.`

--- oauth-server/oauth/src/error-types/index.js
+++ oauth-server/oauth/src/error-types/index.js
@@ -1,4 +1,9 @@
 const InvalidRequestError = require("./invalid-request-error");
 const UnauthorizedClientError = require("./unauthorized-client-error");
+const AccessDeniedError = require("./access-denied-error");
 
-module.exports = { InvalidRequestError, UnauthorizedClientError };
+module.exports = {
+  InvalidRequestError,
+  UnauthorizedClientError,
+  AccessDeniedError
+};
