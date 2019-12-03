ecc4140477bcd29ab9a04c6d4449a9b0675ca293
Logqwerty <meg0070@naver.com>
2019-12-03 18:15:47 +0900

    [Feat] InvalidScopeError 클래스 구현
    Error Response 중 invalid_scope에 대한 Error 클래스 구현 및
    테스트 코드 구현.

--- oauth-server/oauth/src/error-types/index.js
+++ oauth-server/oauth/src/error-types/index.js
@@ -2,10 +2,12 @@ const InvalidRequestError = require("./invalid-request-error");
 const UnauthorizedClientError = require("./unauthorized-client-error");
 const AccessDeniedError = require("./access-denied-error");
 const UnsupportedResonseTypeError = require("./unsupported-response-type-error");
+const InvalidScopeError = require("./invalid-scope-error");
 
 module.exports = {
   InvalidRequestError,
   UnauthorizedClientError,
   AccessDeniedError,
-  UnsupportedResonseTypeError
+  UnsupportedResonseTypeError,
+  InvalidScopeError
 };
