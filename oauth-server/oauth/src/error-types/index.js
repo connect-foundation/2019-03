525f8e4dcfe9bf3983bf4f7829465aa61795bf1d
Logqwerty <meg0070@naver.com>
2019-12-03 18:11:44 +0900

    [Feat] UnsupportedResonseTypeError 클래스 구현
    Error Response 중 unsupported_response_type에 대한 Error 클래스 구현
    및 테스트 코드 구현.

--- oauth-server/oauth/src/error-types/index.js
+++ oauth-server/oauth/src/error-types/index.js
@@ -1,9 +1,11 @@
 const InvalidRequestError = require("./invalid-request-error");
 const UnauthorizedClientError = require("./unauthorized-client-error");
 const AccessDeniedError = require("./access-denied-error");
+const UnsupportedResonseTypeError = require("./unsupported-response-type-error");
 
 module.exports = {
   InvalidRequestError,
   UnauthorizedClientError,
-  AccessDeniedError
+  AccessDeniedError,
+  UnsupportedResonseTypeError
 };
