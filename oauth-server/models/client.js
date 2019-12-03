ab7afb27030d1696c5e4af6fcd4a143722df004b
soyoungjeong <hana46798@gmail.com>
2019-12-03 15:51:02 +0900

    [Feat] authorization server의 sequelize 설정
    client table만들기 위해 authorization server에 sequelize 설정.

---
+++ oauth-server/models/client.js
@@ -0,0 +1,39 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Client = sequelize.define(
+    "Client",
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        autoIncrement: true
+      },
+      clientType: {
+        type: DataTypes.STRING(100)
+      },
+      redirectionURI: {
+        allowNull: true,
+        type: DataTypes.STRING(2048)
+      },
+      appName: {
+        type: DataTypes.STRING(2048)
+      },
+      website: {
+        allowNull: false,
+        type: DataTypes.STRING(2048)
+      },
+      description: {
+        allowNull: false,
+        type: DataTypes.STRING(2048)
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    },
+    {}
+  );
+  Client.associate = function(models) {};
+  return Client;
+};
