c2aa68f0c2c03047a86ebfdfd8d8d178d1a01d59
Logqwerty <meg0070@naver.com>
2019-12-03 18:01:16 +0900

    [Feat] UnauthorizedClientError 클래스 구현
    Error Response 중 unauthorized_client에 대한 Error 클래스 구현 및
    테스트 코드 구현.

--- oauth-server/oauth/src/error-types/index.js
+++ oauth-server/oauth/src/error-types/index.js
@@ -1,3 +1,4 @@
 const InvalidRequestError = require("./invalid-request-error");
+const UnauthorizedClientError = require("./unauthorized-client-error");
 
-module.exports = { InvalidRequestError };
+module.exports = { InvalidRequestError, UnauthorizedClientError };
--- oauth-server/oauth/test/error-types/error-types.test.js
+++ oauth-server/oauth/test/error-types/error-types.test.js
@@ -1,4 +1,7 @@
-const { InvalidRequestError } = require("../../src/error-types");
+const {
+  InvalidRequestError,
+  UnauthorizedClientError
+} = require("../../src/error-types");
 
 describe("OAuth 2.0 Error Response Type 테스트", () => {
   describe("InvalidRequestError 테스트", () => {
@@ -8,6 +11,17 @@ describe("OAuth 2.0 Error Response Type 테스트", () => {
 
       expect(error.message).toEqual(message);
       expect(error.name).toEqual("invalid_request");
+      expect(error.status).toEqual(400);
+    });
+  });
+
+  describe("UnauthorizedClientError 테스트", () => {
+    test("UnauthorizedClientError 생성자 테스트", () => {
+      const message = "This is a test!";
+      const error = new UnauthorizedClientError(message);
+
+      expect(error.message).toEqual(message);
+      expect(error.name).toEqual("unauthorized_client");
       expect(error.status).toEqual(400);
     });
   });
