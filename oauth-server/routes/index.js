2f0890dfb7056bde2be0f87281ff6037c5e82a7b
soyoungjeong <hana46798@gmail.com>
2019-12-03 23:05:41 +0900

    [Feat] client registration 기능 구현
    client registration 기능 구현하며 과정 중에 발생하는 error type 작성.
    해당 함수의 test code 작성.

--- oauth-server/routes/index.js
+++ oauth-server/routes/index.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const registration = require("../services/registration");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
+});
+
+/*
+req.body = {
+  type
+  redirectionURI
+  app_name
+  website
+  description
+}
+*/
+router.get("/register", async function(req, res, next) {
+  const client = req.body;
+  try {
+    const clientInfo = await registration(client);
+    res.json({ data: clientInfo });
+  } catch (e) {
+    if (e.name === "client_error") {
+      res.json({ result: "fail", message: "e.message" });
+    } else {
+      res.json({ result: "fail", message: "server error" });
+    }
+  }
 });
 
 module.exports = router;
