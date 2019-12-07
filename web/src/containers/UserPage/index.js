54622e4f3b92f61b855738b5ae1cfd77e45a6255
JaeHyeonKim19 <JaeHyeonKim19@gmail.com>
2019-12-08 02:35:51 +0900

    [Refact] UserPage Apollo-Client 적용
    기존 custom hooks를 사용하던 것을 apollo/react-hooks로 변경

--- web/src/containers/UserPage/index.js
+++ web/src/containers/UserPage/index.js
@@ -1,34 +1,36 @@
 import React, { useEffect } from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
 import PostCardList from '../../components/PostCardList';
 import UserPageInfo from './UserPageInfo';
 import ListSelector from './ListSelector';
 import { UserPageWrapper, UserPageSection } from './styles';
 
-import { useFetch } from '../../hooks';
-
 const UserPage = ({ match }) => {
   const { username } = match.params;
 
-  const userPageQuery = `{
-    userPage(writer:"${username}"){
-      isExistingUser
-      postCard{
-        postURL
-        imageURL
+  const userPageQuery = gql`
+    query UserPage($writer: String!) {
+      userPage(writer: $writer) {
+        isExistingUser
+        postCard {
+          postURL
+          imageURL
+        }
       }
     }
-  }`;
+  `;
 
-  const { state, fetchData } = useFetch();
+  const { loading, error, data, refetch } = useQuery(userPageQuery, {
+    variables: { writer: username },
+  });
   useEffect(() => {
-    fetchData(userPageQuery);
+    refetch();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [userPageQuery]);
-  const { loading, data, error } = state;
+  }, [username]);
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
-  if (!data) return null;
   if (!data.userPage.isExistingUser) return <div>존재하지않는 유저입니다.</div>;
   return (
     <UserPageWrapper>
