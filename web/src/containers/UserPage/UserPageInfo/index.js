4461bf6af0f62efb3f236370be3755bdcb6159f0
JaeHyeonKim19 <JaeHyeonKim19@gmail.com>
2019-12-09 02:45:14 +0900

    [Impv] FollowButton 정상 작동을 위한 UserPage props 전달
    UserPage에서 FollowButton을 정상작동 시키기 위해 props를 전달

--- web/src/containers/UserPage/UserPageInfo/index.js
+++ web/src/containers/UserPage/UserPageInfo/index.js
@@ -4,13 +4,13 @@ import ProfileIcon from '../../../components/ProfileIcon';
 import UserInfo from './UserInfo';
 import { UserPageInfoWrapper, ProfileIconWrapper } from './styles';
 
-const UserPageInfo = ({ username, data }) => {
+const UserPageInfo = ({ username, myId, data }) => {
   return (
     <UserPageInfoWrapper>
       <ProfileIconWrapper>
         <ProfileIcon ratio={46.875} />
       </ProfileIconWrapper>
-      <UserInfo username={username} data={data} />
+      <UserInfo username={username} myId={myId} data={data} />
     </UserPageInfoWrapper>
   );
 };
