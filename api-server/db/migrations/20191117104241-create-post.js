1ee759bca352ab963f56741341ff4c5e84fced3d
Jack <rainy_waltz@naver.com>
2019-12-09 11:48:58 +0900

    [Fix] 데이터 베이스 수정
    Index와 제약 조건 추가.

--- api-server/db/migrations/20191117104241-create-post.js
+++ api-server/db/migrations/20191117104241-create-post.js
@@ -1,27 +1,29 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Posts', {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-      },
-      postURL: {
-        type: Sequelize.STRING(2048),
-      },
-      imageURL: {
-        allowNull: true,
-        type: Sequelize.STRING(2048),
-      },
-      content: {
-        type: Sequelize.STRING(1000),
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
+    return queryInterface
+      .createTable('Posts', {
+        id: {
+          allowNull: false,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+          autoIncrement: true,
+        },
+        postURL: {
+          type: Sequelize.STRING(2048),
+        },
+        imageURL: {
+          allowNull: true,
+          type: Sequelize.STRING(2048),
+        },
+        content: {
+          type: Sequelize.STRING(1000),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      })
+      .then(() => queryInterface.addIndex('Posts'), ['postURL']);
   },
   down: queryInterface => {
     return queryInterface.dropTable('Posts');
