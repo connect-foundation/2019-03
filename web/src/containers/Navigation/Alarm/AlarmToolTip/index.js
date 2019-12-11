9f5b062861eac003df293984f5e4cbab92c54fa0
unknown <hana46798@gmail.com>
2019-12-11 15:23:06 +0900

    [Refact] 알람 툴팁 결과 보여주는 분기문 수정
    분기문 중에 중복인 부분이 많아 줄이기 위해 수정.

--- web/src/containers/Navigation/Alarm/AlarmToolTip/index.js
+++ web/src/containers/Navigation/Alarm/AlarmToolTip/index.js
@@ -1,42 +1,38 @@
 import React from 'react';
 import { AlarmToolTipWrapper, AlarmResultList, AlarmNoResult } from './styles';
 
+const ARROW_MOVEMENT = '85%';
+
 const AlarmToolTip = ({ isVisible, setIsVisible, data, loading, error }) => {
+  let content = '';
+  let resultList = <AlarmNoResult>{content}</AlarmNoResult>;
+
   const clickClose = () => {
     setIsVisible(false);
   };
+
   if (!isVisible) return null;
   if (loading) {
-    return (
-      <AlarmToolTipWrapper arrowStyle={{ left: '85%' }} onClick={clickClose}>
-        <AlarmNoResult>
-          <span>로딩중.</span>
-        </AlarmNoResult>
-      </AlarmToolTipWrapper>
-    );
+    content = <span>로딩중.</span>;
   }
   if (error) {
-    return (
-      <AlarmToolTipWrapper arrowStyle={{ left: '85%' }} onClick={clickClose}>
-        <AlarmNoResult>
-          <span>에러가 발생했습니다.</span>
-        </AlarmNoResult>
-      </AlarmToolTipWrapper>
-    );
+    content = <span>에러가 발생했습니다.</span>;
   }
-  const { log } = data;
-  if (log.length === 0) {
-    return (
-      <AlarmToolTipWrapper arrowStyle={{ left: '85%' }} onClick={clickClose}>
-        <AlarmNoResult>
-          <span>새로운 알림이 없습니다.</span>
-        </AlarmNoResult>
-      </AlarmToolTipWrapper>
-    );
+  if (data) {
+    const { log } = data;
+    if (log.length === 0) {
+      content = <span>새로운 알림이 없습니다.</span>;
+    } else {
+      resultList = <AlarmResultList alarmResults={log} />;
+    }
   }
+
   return (
-    <AlarmToolTipWrapper arrowStyle={{ left: '85%' }} onClick={clickClose}>
-      <AlarmResultList alarmResults={log} />
+    <AlarmToolTipWrapper
+      arrowStyle={{ left: ARROW_MOVEMENT }}
+      onClick={clickClose}
+    >
+      {resultList}
     </AlarmToolTipWrapper>
   );
 };
