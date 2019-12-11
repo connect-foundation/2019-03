6ef3e7613a300a5fdb01a0765652dfe688f0ff6c
Jack <rainy_waltz@naver.com>
2019-12-11 18:09:34 +0900

    [Refact] 코드 정리
    쿼리문을 queries 디렉토리로 이동
    loading 자리에 Loading 컴포넌트 배치

--- web/src/containers/PostDetailPage/index.js
+++ web/src/containers/PostDetailPage/index.js
@@ -1,32 +1,19 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
 import { PostDetailPageWrapper, ViewPort } from './styles';
 import PostSideBox from './PostSideBox';
 import { PostProvider } from './context';
-import ErrorWrapper from '../ErrorPage/ErrorWrapper';
+import { POST_DETAIL } from './queries';
 import ErrorPage from '../ErrorPage';
+import Loading from '../../components/Loading';
 
 function PostDetailPage({ match }) {
-  const POST_DETAIL = gql`
-    query Post($postURL: String!) {
-      post(postURL: $postURL) {
-        id
-        content
-        writer {
-          username
-        }
-        imageURL
-        postURL
-      }
-    }
-  `;
   const { loading, error, data } = useQuery(POST_DETAIL, {
     variables: { postURL: match.params.postURL },
   });
 
-  if (loading) return <div>로딩중..</div>;
+  if (loading) return <Loading size={50} />;
   if (error) return <ErrorPage />;
   if (!data) return null;
   const { post } = data;
