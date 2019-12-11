ea2af1d3e4e361f764009b23cb5a6abcdbea7e06
unknown <hana46798@gmail.com>
2019-12-11 14:22:09 +0900

    [Fix] 검색 결과 툴팁에 링크 연결
    검색 결과를 보여주는 툴팁에서 유저페이지나 해시태그 페이지로
    이동이 불가능한 버그가 있어 결과에 맞는 링크를 연결.
    
    closed #291

--- web/src/containers/Navigation/Search/SearchToolTip/SearchResult/index.js
+++ web/src/containers/Navigation/Search/SearchToolTip/SearchResult/index.js
@@ -1,40 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import ProfileIcon from '../../../../../components/ProfileIcon';
+import StyledLink from '../../../../../components/StyledLink';
 import { SearchResultWrapper, ResultInfo } from './styles';
 
-const SearchResult = ({ result, isLast }) => {
+const SearchResult = ({ result, isLast, clickClose }) => {
   const [resultInfo, setResultInfo] = useState({
-    imgSrc: null,
+    imageURL: null,
     content: null,
     option: null,
+    linkURL: `/`,
   });
 
   useEffect(() => {
     if (!result.username) {
       setResultInfo({
-        imgSrc: 'https://kr.object.ncloudstorage.com/youngstargram/hashtag.png',
+        imageURL:
+          'https://kr.object.ncloudstorage.com/youngstargram/hashtag.png',
         content: `# ${result.name}`,
         option: null,
+        linkURL: `/h/${result.name}`,
       });
     } else {
       setResultInfo({
-        imgSrc: undefined, // 나중에 user.profileImage로 변경
+        imageURL: result.profileImage,
         content: result.username,
         option: <span className="option">{result.name}</span>,
+        linkURL: `/${result.username}`,
       });
     }
   }, [result, setResultInfo]);
 
   return (
-    <SearchResultWrapper isLast={isLast}>
-      <div>
-        <ProfileIcon imageURL={resultInfo.imgSrc} />
-      </div>
-      <ResultInfo>
-        <span>{resultInfo.content}</span>
-        {resultInfo.option}
-      </ResultInfo>
-    </SearchResultWrapper>
+    <StyledLink to={resultInfo.linkURL} onClick={clickClose}>
+      <SearchResultWrapper isLast={isLast}>
+        <div>
+          <ProfileIcon imageURL={resultInfo.imageURL} />
+        </div>
+        <ResultInfo>
+          <span>{resultInfo.content}</span>
+          {resultInfo.option}
+        </ResultInfo>
+      </SearchResultWrapper>
+    </StyledLink>
   );
 };
 
