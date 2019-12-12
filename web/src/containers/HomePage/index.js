084d33d00a27e19c0e8a078c6df9b62c9bdfb6f2
Jack <rainy_waltz@naver.com>
2019-12-12 13:28:43 +0900

    [Feat] fetch 타이밍 추가
    스크롤 중간에서 추가 데이터 요청.

--- web/src/containers/HomePage/index.js
+++ web/src/containers/HomePage/index.js
@@ -8,19 +8,21 @@ import { FOLLOWING_POST_LIST } from './queries';
 import Spinner from '../../components/Spinner';
 
 const myInfo = {
-  id: 2,
+  id: 1,
   username: '_so_02',
   name: '정소영',
   profileImage: 'https://i.pravatar.cc/150?img=9',
 };
 
+const LIMIT = 5;
 function HomePage() {
   const [noMorePost, setNoMorePost] = useState(false);
   const lastChild = useRef();
+  const middleChild = useRef();
   const { data, loading, error, fetchMore } = useQuery(FOLLOWING_POST_LIST, {
     variables: {
       myId: myInfo.id,
-      limit: 5,
+      limit: LIMIT,
     },
     fetchPolicy: 'cache-and-network',
     notifyOnNetworkStatusChange: true,
@@ -32,36 +34,39 @@ function HomePage() {
   };
 
   const getMorePosts = entries => {
-    const lastPost = [...entries].pop();
     if (loading) return;
-    if (!lastPost.isIntersecting) return;
-
-    const list = data.followingPostList;
-    const cursor = list.length ? [...list].pop().updatedAt : null;
-
-    fetchMore({
-      variables: {
-        cursor,
-      },
-      updateQuery: (prev, { fetchMoreResult }) => {
-        if (!fetchMoreResult) return prev;
-        if (!fetchMoreResult.followingPostList.length) setNoMorePost(true);
-        return {
-          ...prev,
-          followingPostList: [
-            ...prev.followingPostList,
-            ...fetchMoreResult.followingPostList,
-          ],
-        };
-      },
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) return;
+      const list = data.followingPostList;
+      const cursor = list.length ? [...list].pop().updatedAt : null;
+      fetchMore({
+        variables: {
+          cursor,
+        },
+        updateQuery: (prev, { fetchMoreResult }) => {
+          if (!fetchMoreResult) return prev;
+          if (!fetchMoreResult.followingPostList.length) setNoMorePost(true);
+          return {
+            ...prev,
+            followingPostList: [
+              ...prev.followingPostList,
+              ...fetchMoreResult.followingPostList,
+            ],
+          };
+        },
+      });
     });
   };
 
   const { followingPostList } = data || { followingPostList: [] };
 
-  const postList = followingPostList.map(post => (
-    <Post key={post.id} post={post} myInfo={myInfo} />
-  ));
+  const postList = followingPostList.map((post, index) =>
+    index === followingPostList.length - 3 ? (
+      <Post key={post.id} post={post} myInfo={myInfo} ref={middleChild} />
+    ) : (
+      <Post key={post.id} post={post} myInfo={myInfo} />
+    ),
+  );
 
   const spinner = noMorePost ? (
     <div>더 이상 게시글이 없습니다.</div>
@@ -75,6 +80,7 @@ function HomePage() {
     if (noMorePost) return;
     const observer = new IntersectionObserver(getMorePosts, options);
     observer.observe(lastChild.current);
+    if (middleChild.current) observer.observe(middleChild.current);
 
     // eslint-disable-next-line consistent-return
     return () => observer.disconnect();
