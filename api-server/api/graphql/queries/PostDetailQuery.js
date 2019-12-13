d653ed8dbc2f88627b1cd979528c09d05b7a6f1c
Jack <rainy_waltz@naver.com>
2019-12-13 11:55:31 +0900

    [Fix] 상세 페이지 요청 수정
    상세 페이지를 요청할 때에 사용자의 id를 함께 넘겨주는 것으로 수정.

--- api-server/api/graphql/queries/PostDetailQuery.js
+++ api-server/api/graphql/queries/PostDetailQuery.js
@@ -1,4 +1,4 @@
-const { GraphQLString } = require('graphql');
+const { GraphQLString, GraphQLInt } = require('graphql');
 
 const { PostType } = require('../types');
 const { Post } = require('../../../db');
@@ -6,11 +6,14 @@ const { Post } = require('../../../db');
 const postDetailQuery = {
   type: PostType,
   args: {
+    id: { type: GraphQLInt },
     postURL: { type: GraphQLString },
   },
-  resolve: (post, args) => {
+  resolve: (_, { id, postURL }, context) => {
+    // eslint-disable-next-line no-param-reassign
+    context.UserId = id;
     return Post.findOne({
-      where: { postURL: args.postURL },
+      where: { postURL },
     });
   },
 };
