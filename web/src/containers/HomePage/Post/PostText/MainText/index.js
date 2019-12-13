f2085230baee7862535493315c5b84add06d5e6d
Logqwerty <meg0070@naver.com>
2019-12-13 11:46:34 +0900

    [Feat] 해시/사람 태그 구현
    게시글 본문에 해시태그나 사람태그가 있을 경우, 이를
    눌렀을 때 특정 URL로 리다이렉션되도록 본문 내용을 바꾸는
    기능 구현.

--- web/src/containers/HomePage/Post/PostText/MainText/index.js
+++ web/src/containers/HomePage/Post/PostText/MainText/index.js
@@ -7,16 +7,18 @@ import {
   Writer,
 } from '../styles';
 import { useText as useMainText } from '../hooks';
+import { parseMainText, makeMainText } from './lib';
 
 const MainText = ({ writer, mainText }) => {
   const { username } = writer;
   const [isFold, mainTextRef, onUnfoldMainText] = useMainText();
+  const parseResult = parseMainText(mainText);
 
   return (
     <MainTextWrapper style={{ marginBottom: '4px' }}>
       <Writer to={`/${username}`}>{username}</Writer>
       <MainTextContent isFold={isFold} ref={mainTextRef}>
-        {mainText}
+        {parseResult.map(content => makeMainText(content))}
       </MainTextContent>
       {isFold && (
         <MaintextMoreButton onClick={onUnfoldMainText}>
