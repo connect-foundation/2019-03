164e0e924096cad457ce4930c675de0e67a1439a
Jack <rainy_waltz@naver.com>
2019-12-14 20:04:56 +0900

    [Fix] 댓글 길게 작성할 시 생기는 가로 스크롤 처리
    close #346

--- web/src/containers/PostDetailPage/PostSideBox/PostContent/CommentList/Comment/Content/index.js
+++ web/src/containers/PostDetailPage/PostSideBox/PostContent/CommentList/Comment/Content/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 
 import { ContentWrapper } from './styles';
-import StyledLink from '../../../../../../../components/StyledLink';
+import Username from './Username';
 import TimePassed from '../../../../../../../components/TimePassed';
 
 function Content({ comment }) {
   const { writer, content, updatedAt } = comment;
   return (
     <ContentWrapper>
-      <main>
-        <h3>
-          <StyledLink
-            to={`/${writer.username}`}
-            style={{ display: 'inline-block' }}
-          >
-            {writer.username}
-          </StyledLink>
-        </h3>
-        <article>{content}</article>
-      </main>
+      <div>
+        <Username writer={writer} />
+        <article style={{ wordBreak: 'break-all' }}>{content}</article>
+      </div>
       <TimePassed updatedAt={updatedAt} />
     </ContentWrapper>
   );
