dd0a2d8df899f818e58ede95b96f74d5f65276f9
Jack <rainy_waltz@naver.com>
2019-12-17 11:29:56 +0900

    [Refact] PostTop에 있던 query 이동
    PostTop에 있던 queries를 루트 queries 디렉토리로 이동.

--- web/src/queries/index.js
+++ web/src/queries/index.js
@@ -1,16 +1,17 @@
+import UPLOAD_POST from './uploadPost';
 import FOLLOWING_POST_LIST from './followingPostList';
+import READ_POST from './readPost';
+import READ_POST_SIMPLE from './readPostSimple';
+import UPDATE_POST from './updatePost';
+import DELETE_POST from './deletePost';
+import COMMENT_LIST from './commentList';
 import CREATE_COMMENT from './createComment';
 import CREATE_POST_LIKE from './createPostLike';
 import DELETE_POST_LIKE from './deletePostLike';
-import COMMENT_LIST from './commentList';
-import READ_POST from './readPost';
-import READ_POST_SIMPLE from './readPostSimple';
+import SEARCH from './search';
+import GET_LOGS from './getLogs';
 import UPDATE_USER from './updateUser';
 import LIKER_LIST from './likerList';
-import GET_LOGS from './getLogs';
-import SEARCH from './search';
-import UPDATE_POST from './updatePost';
-import UPLOAD_POST from './uploadPost';
 import UPDATE_PROFILE from './updateProfile';
 
 export {
@@ -28,4 +29,5 @@ export {
   UPDATE_POST,
   UPLOAD_POST,
   UPDATE_PROFILE,
+  DELETE_POST,
 };
