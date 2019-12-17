be7eac7ee18b98e22d0ba7e5d6a8e2a3241d7a86
Logqwerty <meg0070@naver.com>
2019-12-17 16:36:47 +0900

    [Feat] OAuth 서비스에 필요한 모델 생성
    OAuth 클라이언트를 등록할 때 필요한 Client 모델과 발급된
    Token을 관리할 때 필요한 Token 모델 생성.

---
+++ api-server/db/migrations/20191217072900-create-client.js
@@ -0,0 +1,39 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Clients', {
+      clientId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING(256),
+      },
+      clientType: {
+        allowNull: false,
+        type: Sequelize.STRING(20),
+      },
+      redirectionURI: {
+        allowNull: false,
+        type: Sequelize.STRING(2048),
+      },
+      appName: {
+        type: Sequelize.STRING(50),
+      },
+      website: {
+        type: Sequelize.STRING(2048),
+      },
+      description: {
+        type: Sequelize.STRING(500),
+      },
+      clientSecret: {
+        type: Sequelize.STRING(256),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE(3),
+        defaultValue: new Date(),
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Clients');
+  },
+};
