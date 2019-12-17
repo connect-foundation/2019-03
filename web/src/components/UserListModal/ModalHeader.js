4977eaf53b7f8e6bb19b56406339670643dae7a7
JaeHyeonKim19 <JaeHyeonKim19@gmail.com>
2019-12-17 17:22:35 +0900

    [Refact] UserListModal의 ModalHeader css 순서 정리
    css컨벤션의 순서에 맞춰 순서 수정

--- web/src/components/UserListModal/ModalHeader.js
+++ web/src/components/UserListModal/ModalHeader.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 const ModalHeader = styled.div`
-  height: 43px;
   display: flex;
   justify-content: center;
   align-items: center;
+  height: 43px;
   border-bottom: 1px solid ${({ theme }) => theme.palette.border_secondary};
   h1 {
     margin: 0;
