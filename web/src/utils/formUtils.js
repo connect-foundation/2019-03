adba7dbd5257d08ee5c162e736a30edc7d955cc0
Logqwerty <meg0070@naver.com>
2019-12-19 15:09:52 +0900

    [Fix] 로그인/회원가입 버그 수정
    safari에서 로그인/회원가입 폼이 제대로 fetch되지 않는
    버그 수정.

--- web/src/utils/formUtils.js
+++ web/src/utils/formUtils.js
@@ -7,14 +7,24 @@ function setFormData(form, name, value) {
   form.elements.namedItem(name).value = value;
 }
 
-function changeToFormDataFormat(form, password) {
-  const formData = Object.entries(form.elements).reduce((result, elem) => {
-    if (elem[1].tagName === 'BUTTON') return result;
-    const input = elem[1];
-    if (input.name === 'password') return result;
-    return `${result}${input.name}=${input.value}&`;
-  }, '');
-  return formData.concat(`password=${password}`);
+function changeToFormDataFormat(form, password, isSignin = true) {
+  // const formData = Object.entries(form.elements).reduce((result, elem) => {
+  //   if (elem[1].tagName === 'BUTTON') return result;
+  //   const input = elem[1];
+  //   if (input.name === 'password') return result;
+  //   return `${result}${input.name}=${input.value}&`;
+  // }, '');
+
+  const username = getFormData(form, 'username');
+  const formData = `username=${username}&password=${password}`;
+  if (isSignin) return formData;
+
+  const name = getFormData(form, 'name');
+  const email = getFormData(form, 'email');
+  const cellPhone = getFormData(form, 'cellPhone');
+  formData.concat(`&name=${name}&email=${email}&cellPhone=${cellPhone}`);
+
+  return formData;
 }
 
 export { getFormData, setFormData, changeToFormDataFormat };
