82042d54d09e191ac53b960dcb7a2022f41166fa
Jack <rainy_waltz@naver.com>
2019-12-19 21:59:12 +0900

    [Refact] 리스트 content 함수화
    리스트에 들어가는 내용을 함수화 시킴

--- web/src/components/UserListModal/index.js
+++ web/src/components/UserListModal/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
+import { List } from 'react-virtualized';
 
 import Modal from '../Modal';
 import Spinner from '../Spinner';
@@ -7,23 +8,26 @@ import NoResult from './NoResult';
 import UserListItem from './UserListItem';
 import { ModalHeader, ModalContent } from './styles';
 
+const createContent = ({ myId, listName, data, error }) => {
+  const spinnerSize = 100;
+  if (error) return <div>다시 시도해주세요.</div>;
+  if (!data) return <Spinner size={spinnerSize} />;
+  const DataKeys = Object.keys(data);
+  const list = data[DataKeys[0]];
+  if (!list.length) return <NoResult listName={listName} />;
+  return list.map(user => (
+    <UserListItem userInfo={user} myId={myId} key={user.id} />
+  ));
+};
+
 const UserListModal = ({ myId, onClick, listName, query, userId }) => {
   const queryOption = {
     variables: { myId, userId },
     fetchPolicy: 'cache-and-network',
   };
-  const { data, loading, error } = useQuery(query, queryOption);
-  const spinnerSize = 100;
-  let content = <NoResult listName={listName} />;
-  if (error) content = <div>다시 시도해주세요.</div>;
-  if (loading) content = <Spinner size={spinnerSize} />;
-  if (data) {
-    const DataKeys = Object.keys(data);
-    if (data[DataKeys[0]].length !== 0)
-      content = data[DataKeys[0]].map(user => (
-        <UserListItem userInfo={user} myId={myId} key={user.id} />
-      ));
-  }
+  const { data, error } = useQuery(query, queryOption);
+  const content = createContent({ myId, listName, data, error });
+
   return (
     <Modal onClick={onClick}>
       <ModalHeader>
