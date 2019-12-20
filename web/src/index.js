c20b88e8523e9b67ff3df390f93cb54d8019f2eb
unknown <hana46798@gmail.com>
2019-12-20 14:01:31 +0900

    [Feat] 전체 페이지 폰트 적용
    모든 브라우저에서 동일한 폰트가 보이도록 폰트 적용.

--- web/src/index.js
+++ web/src/index.js
@@ -28,10 +28,17 @@ const client = new ApolloClient({
 });
 
 const GlobalStyle = createGlobalStyle`
+    @font-face {
+      font-family: 'NotoSansKR';
+      font-weight: 300;
+      src: local('NotoSansKR'), local('NotoSansKR'),
+           url('./fonts/NotoSansKR-Light.otf') format('otf'), 
+    }
     body {
         padding: 0;
         margin: 0 auto;
         background-color:#fafafa;
+        font-family:'NotoSansKR'
     }
 `;
 
