a32698420832735ddda83683a25238e3b4e2aecd
Logqwerty <meg0070@naver.com>
2019-12-22 11:06:23 +0900

    [Feat] client 등록 HTTP method 변경
    client 등록 HTTP method를 POST로 변경

--- oauth-server/routes/client.js
+++ oauth-server/routes/client.js
@@ -2,11 +2,11 @@ const express = require("express");
 const client = express.Router();
 const { register } = require("../services/client-service");
 
-client.get("/register", async function(req, res, next) {
+client.post("/register", async function(req, res, next) {
   try {
     const client = req.body;
     await register(client);
-    res.json({ result: "success" });
+    res.json({ status: "success", message: "client registration success." });
   } catch (err) {
     next(err);
   }
